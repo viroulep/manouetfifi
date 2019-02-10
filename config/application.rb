128b49b18f9698dfcbe715fe9cece9a666d0e571
Philippe Virouleau <philippe.44@gmail.com>
2019-02-10 01:03:20 +0100

    i18n location

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-require_relative 'locales/locales'
 
 require 'rails/all'
 
@@ -16,8 +15,5 @@ module Manouetfifi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    I18n.available_locales = Locales::AVAILABLE.keys
-    config.i18n.default_locale = :en
   end
 end
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,5 @@
-I18n.config.available_locales = [:fr, :en]
+require_relative '../locales/locales'
+
+I18n.config.available_locales = Locales::AVAILABLE.keys
 Rails.application.config.i18n.default_locale = :fr
-Rails.application.config.i18n.fallbacks = [:en]
+Rails.application.config.i18n.fallbacks = [:fr]
