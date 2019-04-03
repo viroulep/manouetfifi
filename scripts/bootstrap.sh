ed2ee2b777f0592943ed2f2bfe55ebcd2fa5a089
Philippe Virouleau <philippe.44@gmail.com>
2019-04-03 15:52:33 +0200

    fix cert

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -83,7 +83,7 @@ if [ "x$user_choice" == "xy" ]; then
 fi
 
 echo "Installing cron scripts"
-cp /home/fifi/manouetfifi/scripts/cron_weekly.sh /etc/cron.weekly
+cp /home/fifi/manouetfifi/scripts/cert_nginx /etc/cron.weekly
 
 echo "Bootstraping as FiFi"
 su fifi -c "cd /home/fifi && /home/fifi/manouetfifi/scripts/manouetfifi_bootstrap.sh"
